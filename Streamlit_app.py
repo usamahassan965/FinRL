c92f557f3e4d5d47b781d561bae02776faf03487
Usama Hassan <96824810+usamahassan965@users.noreply.github.com>
2023-11-05 00:18:58 +0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -174,8 +174,8 @@ def plot_data(df_unique, Use_ticker):
 
     fig.update_layout(height=900, width=1100)
 
-    fig.show()
-    st.plotly_chart(fig)
+    #fig.show()
+    st.plotly_chart(fig,use_container_width=True)
 
 
 if df_unique is not None and Plot_data:
